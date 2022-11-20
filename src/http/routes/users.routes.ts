4615c7f7c673acdf157e8590b026ca54417d813d
Murilo Analiel Machado <muriloam@outlook.com>
2022-11-20 01:05:20 -0300

    Cria rota de get dos dados do usuário

--- src/http/routes/users.routes.ts
+++ src/http/routes/users.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 
 import { createUserController } from "../controllers/createUser";
 import { addDefeatController } from "../controllers/defeats";
+import { getUserController } from "../controllers/getUser";
 import { loginUserController } from "../controllers/LoginUser";
 import { addWinController } from "../controllers/wins";
 
@@ -21,6 +22,10 @@ usersRoutes.post("/wins/:username", async (request, response) => {
 
 usersRoutes.post("/defeats/:username", async (request, response) => {
     await addDefeatController.handle(request, response);
+});
+
+usersRoutes.get("/:username", async (request, response) => {
+    await getUserController.handle(request, response);
 });
 
 export { usersRoutes };
